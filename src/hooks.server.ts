39533900eb7515fd4aa0abcfe8fd06fb17884d25
hAbuMustafa <hosam5553@gmail.com>
2026-04-28 23:18:32 +0300

    embed caching script in hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,13 @@
+import { initialize } from "$lib/server/db/init";
+
+let initialized = false;
+
 export async function handle({ event, resolve }) {
+  if (!initialized) {
+    await initialize();
+
+    initialized = true;
+  }
+
   return await resolve(event);
 }
