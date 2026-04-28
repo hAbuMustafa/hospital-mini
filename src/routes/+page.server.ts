a520bffe9fa92abcb44426b50940e5a97a86596c
hAbuMustafa <hosam5553@gmail.com>
2026-04-29 02:51:10 +0300

    more precise date handling

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -12,18 +12,32 @@ export async function load({ url }) {
   const dischargesFrom = url.searchParams.get("f") ?? yesterdayString;
   const dischargesTo = url.searchParams.get("t") ?? yesterdayString;
 
+  const dateFrom = new Date(dischargesFrom);
+  const dateTo = new Date(dischargesTo);
+  dateFrom.setHours(0);
+  dateFrom.setMinutes(0);
+  dateFrom.setSeconds(0);
+  dateFrom.setMilliseconds(0);
+
+  dateTo.setHours(23);
+  dateTo.setMinutes(59);
+  dateTo.setSeconds(59);
+  dateTo.setMilliseconds(999);
+
   const fetchedPatients = await db
     .select()
     .from(patients)
     .where(
       and(
-        gte(patients.discharge_date, new Date(dischargesFrom)),
-        lte(patients.discharge_date, new Date(dischargesTo)),
+        gte(patients.discharge_date, dateFrom),
+        lte(patients.discharge_date, dateTo),
       ),
     )
     .orderBy(patients.admission_date);
 
   return {
     patients: fetchedPatients,
+    dateFrom: `${dateFrom.getFullYear()}-${`${dateFrom.getMonth() + 1}`.padStart(2, "0")}-${`${dateFrom.getDate()}`.padStart(2, "0")}`,
+    dateTo: `${dateTo.getFullYear()}-${`${dateTo.getMonth() + 1}`.padStart(2, "0")}-${`${dateTo.getDate()}`.padStart(2, "0")}`,
   };
 }
