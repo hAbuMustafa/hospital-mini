5b6527926d599af5704c1b24d779ff489b3ba40c
hAbuMustafa <hosam5553@gmail.com>
2026-04-28 23:18:45 +0300

    schema for local db

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,38 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, real, int } from "drizzle-orm/sqlite-core";
 
-export const task = sqliteTable('task', {
-	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+export const drugs = sqliteTable("drugs", {
+  name_ar: text(),
+  unit: text(),
+  tradename_ar: text(),
+  name: text(),
+  price: real(),
+  price_resale: real(),
+  record_4_page: int(),
+  stock_amount: int(),
+  query_in_UPA_sheet: text(),
+  occurences_in_UPA_sheet: int(),
+  category: text(),
+  id: int().primaryKey(),
+  record_2_page: int(),
+  smc_code: int(),
+  is_used: text(),
+  tradename: text(),
+});
+
+export const patients = sqliteTable("patients", {
+  id: text().primaryKey(),
+  name: text(),
+  id_type: text(),
+  id_number: text(),
+  diagnosis: text(),
+  admission_date: text(),
+  discharge_date: text(),
+  discharge_reason: text(),
+  ward_recent: text(),
+  ward_on_admission: text(),
+  admission_notes: text(),
+  ininininini: text(), // placeholder for seeding of form submitted "is insured"
+  gender: int(),
+  birthdate: text(),
+  insured: int(),
 });
