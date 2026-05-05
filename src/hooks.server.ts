cb49b66c284b28569ad4ef3d9ec8a9269af428bc
hAbuMustafa <hosam5553@gmail.com>
2026-05-05 03:14:13 +0300

    do not seed on first run

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,3 @@
-import { initialize } from "$lib/server/db/init";
-
-let initialized = false;
-
 export async function handle({ event, resolve }) {
-  if (!initialized) {
-    await initialize();
-
-    initialized = true;
-  }
-
   return await resolve(event);
 }
