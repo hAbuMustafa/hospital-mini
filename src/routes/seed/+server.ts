c546c5c608ee5769cac337d5659df654b1fde821
hAbuMustafa <hosam5553@gmail.com>
2026-05-05 05:42:03 +0300

    use GET

--- src/routes/seed/+server.ts
+++ src/routes/seed/+server.ts
@@ -2,10 +2,14 @@ import { json } from "@sveltejs/kit";
 import { seed_pw } from "$env/static/private";
 import { initialize } from "$lib/server/db/init.js";
 
-export async function POST({ request }) {
-  const { pw } = await request.json();
+export async function GET({ url }) {
+  const pw = url.searchParams.get("pw");
 
-  if (!pw && pw !== seed_pw) return json("SURE THING! All seeding is skipped 🫡");
+  if (!pw || pw !== seed_pw) return json("SURE THING! All seeding is skipped 🫡");
+
+  console.log("🌱 Seeding Started!");
 
   await initialize();
+
+  return json("✔️ ALL SEEDING IS DONE!");
 }
