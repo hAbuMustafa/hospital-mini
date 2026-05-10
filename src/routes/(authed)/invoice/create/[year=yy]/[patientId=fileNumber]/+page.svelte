8f9a5427d0208e77aaf053c02b4d03c00e773634
hAbuMustafa <hosam5553@gmail.com>
2026-05-10 15:31:16 +0300

    pricing range

--- src/routes/(authed)/invoice/create/[year=yy]/[patientId=fileNumber]/+page.svelte
+++ src/routes/(authed)/invoice/create/[year=yy]/[patientId=fileNumber]/+page.svelte
@@ -6,8 +6,12 @@
 
   const today = getToday();
 
-  const fromDate = $derived(patient.admission_date);
-  const toDate = $derived(patient.discharge_date);
+  let fromDate = $derived(formatDate(patient.admission_date, "yyyy-MM-dd"));
+  let toDate = $derived(formatDate(patient.discharge_date, "yyyy-MM-dd"));
+
+  let periodSameAsStay = $derived(
+    fromDate === patient.admission_date && toDate === patient.discharge_date,
+  );
 
   const pricingDuration = $derived(
     getTermed(getDuration(fromDate, toDate || today), "يوم", "أيام"),
@@ -16,7 +20,7 @@
 
 <header>
   <h1>فاتورة أدوية</h1>
-  <table>
+  <table class="patient-data">
     <tbody>
       <tr>
         <th>رقم القيد:</th>
@@ -54,19 +58,66 @@
       </tr>
     </tbody>
   </table>
-
+  <details open={true}>
+    <summary>فترة تسعير</summary>
+    <table class="pricing-range">
+      <!-- bug: dates aren't synced -->
+      <thead>
+        <tr>
+          <th colspan="4">فترة التسعير</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>من:</th>
+          <td>
+            <input type="date" bind:value={fromDate} />
+          </td>
+          <th>إلى:</th>
+          <td>
+            <input type="date" bind:value={toDate} />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </details>
   <h2>سداد فاتورة</h2>
 </header>
 
 <style>
   table {
+    td {
+      text-align: center;
+    }
+  }
+
+  .patient-data {
     th {
       text-align: end;
     }
 
     td {
-      text-align: center;
       padding-inline: 3vw;
+    }
+  }
+
+  .pricing-range {
+    width: 100%;
+    border-collapse: collapse;
+
+    &,
+    th,
+    td {
+      border: var(--main-border);
+    }
+
+    thead {
+      background-color: var(--main-table-header-bg-color);
+    }
+
+    input[type="date"] {
+      width: 80%;
+      text-align: center;
     }
   }
 </style>
