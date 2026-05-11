1faa1568d92acdbff4b610cbef5d7ce3952dce7e
hAbuMustafa <hosam5553@gmail.com>
2026-05-11 17:48:26 +0300

    prefetch stale data

--- src/routes/(authed)/invoice/create/[year=yy]/[patientId=fileNumber]/+page.server.ts
+++ src/routes/(authed)/invoice/create/[year=yy]/[patientId=fileNumber]/+page.server.ts
@@ -1,8 +1,9 @@
+import { formatDate } from "$lib/date/utils";
 import { db } from "$lib/server/db/";
 import { patients_view } from "$lib/server/db/schema";
 import { eq } from "drizzle-orm";
 
-export async function load({ params }) {
+export async function load({ params, fetch }) {
   const patientId = [params.year, params.patientId].join("/");
 
   const [patient] = await db
@@ -10,7 +11,12 @@ export async function load({ params }) {
     .from(patients_view)
     .where(eq(patients_view.id, patientId));
 
+  const staleData = await fetch(
+    `/api/v1/patient/getStaleData?patient_id=${patient.id}&f=${formatDate(patient.admission_date)}&t=${formatDate(patient.discharge_date ?? new Date())}`,
+  ).then((d) => d.json());
+
   return {
     patient,
+    staleData,
   };
 }
