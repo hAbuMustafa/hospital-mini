d59540abdf046c5b0253be8e113fd7663d354bc6
hAbuMustafa <hosam5553@gmail.com>
2026-05-12 00:55:46 +0300

    reformed component

--- src/lib/components/invoice/DrugLookup.svelte
+++ src/lib/components/invoice/DrugLookup.svelte
@@ -1,40 +1,100 @@
 <script lang="ts">
-  import type { drugs } from "$lib/server/db/schema";
   import { debounce } from "lodash-es";
 
   type PropsT = {
-    onSelect: (item: typeof drugs.$inferSelect) => void;
+    list: InvoiceDrugT[];
   };
 
-  let { onSelect } = $props();
+  let { list = $bindable([]) }: PropsT = $props();
 
   let query = $state("");
 
-  let matches: (typeof drugs.$inferInsert)[] = $state([]);
+  let matches: DrugT[] = $state([]);
+
+  function selectDrug(item: InvoiceDrugT) {
+    const foundItemIndexInList = list.findIndex((d) => d.id === item.id);
+    if (foundItemIndexInList > -1) {
+      alert(`الصنف موجود بالفعل في السطر ${foundItemIndexInList + 1}`);
+      return;
+    }
+
+    item.amount = 1;
+    item.total = () => item.amount * (item.price_resale ?? 0);
+    list.push(item);
+  }
 </script>
 
-<input
-  type="search"
-  bind:value={query}
-  oninput={debounce(async () => {
-    if (query === "") return;
+<div class="drug-lookup hide-in-print">
+  <input
+    type="search"
+    placeholder="اسم الصنف (مثلا: بالميكورت أو أوندانسيترون أو adrenaline)"
+    bind:value={query}
+    oninput={debounce(async () => {
+      if (query === "") return;
 
-    matches = await fetch(`/api/v1/drug?q=${query}`).then((d) => d.json());
-  }, 500)}
-/>
-{#if matches.length}
-  <ul class="drug-list">
-    {#each matches as drug, i (drug.id)}
-      <li>
-        <button type="button" onclick={onSelect}>
-          <strong class="name-ar">{drug.name_ar}</strong>
-          <span class="name">{drug.name}</span>
-          <span class="price">{drug.price_resale?.toPrecision(3)} جنيه</span>
-        </button>
-      </li>
-    {/each}
-  </ul>
-{/if}
+      matches = await fetch(`/api/v1/drug?q=${query}`).then((d) => d.json());
+    }, 500)}
+  />
+  {#if matches.length}
+    <ul class="drug-list">
+      {#each matches as drug (drug.id)}
+        <li>
+          <button type="button" onclick={() => selectDrug(drug)}>
+            <strong class="name-ar">{drug.name_ar}</strong>
+            <span class="name">{drug.name}</span>
+            <span class="price">{drug.price_resale?.toFixed(3)} جنيه</span>
+          </button>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</div>
 
 <style>
+  .drug-lookup {
+    input {
+      width: 100%;
+      font-size: 2rem;
+      text-align: center;
+      anchor-name: --drug-lookup-input;
+      margin-block-end: 2rem;
+
+      &::placeholder {
+        font-size: 1rem;
+      }
+    }
+
+    &:not(:focus-within) ul.drug-list {
+      display: none;
+    }
+  }
+
+  ul.drug-list {
+    inset: unset;
+    position: absolute;
+    inset-block-start: calc(anchor(bottom) + 0.25rem);
+    justify-self: anchor-center;
+    position-anchor: --drug-lookup-input;
+    position-try-fallbacks: --bottom-center, --top-center;
+
+    list-style: none;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    border: var(--main-border);
+    box-shadow: var(--main-shadow);
+    margin: 0;
+
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    gap: 0.5rem;
+
+    background-color: var(--menu-bg-color);
+
+    li > button {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+    }
+  }
 </style>
