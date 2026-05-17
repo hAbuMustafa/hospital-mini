935aefcbec19d32145d779ec0960f81fd8db3543
hAbuMustafa <hosam5553@gmail.com>
2026-05-17 13:41:58 +0300

    attach session data

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,5 +3,14 @@ import { building } from "$app/environment";
 import { auth } from "$lib/server/utils/auth";
 
 export async function handle({ event, resolve }) {
+  const session = await auth.api.getSession({
+    headers: event.request.headers,
+  });
+
+  if (session) {
+    event.locals.session = session.session;
+    event.locals.user = session.user;
+  }
+
   return svelteKitHandler({ event, resolve, auth, building });
 }
