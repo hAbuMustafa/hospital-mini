a65f60ee1482695573e6c67796cbd3353bb0fa99
hAbuMustafa <hosam5553@gmail.com>
2026-05-17 13:40:51 +0300

    define types

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,7 +5,27 @@ import type { drugs, narcoticsDispensed } from "$lib/server/db/schema";
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      user: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        email: string;
+        emailVerified: boolean;
+        name: string;
+        image?: string | null | undefined;
+      };
+      session: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        userId: string;
+        expiresAt: Date;
+        token: string;
+        ipAddress?: string | null | undefined;
+        userAgent?: string | null | undefined;
+      };
+    }
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
