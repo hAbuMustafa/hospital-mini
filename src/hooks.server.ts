2a3f7a485d74e5638751a9a55ba27070520f5abe
hAbuMustafa <hosam5553@gmail.com>
2026-05-19 12:54:11 +0300

    assure no resolve without session validation

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,10 +3,6 @@ import { auth } from "$lib/server/utils/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 
 export async function handle({ event, resolve }) {
-  if (event.url.pathname.startsWith("/api/auth")) {
-    return svelteKitHandler({ event, resolve, auth, building });
-  }
-
   const session = await auth.api.getSession({
     headers: event.request.headers,
   });
@@ -14,5 +10,5 @@ export async function handle({ event, resolve }) {
   event.locals.user = session?.user ?? null;
   event.locals.session = session?.session ?? null;
 
-  return resolve(event);
+  return svelteKitHandler({ event, resolve, auth, building });
 }
