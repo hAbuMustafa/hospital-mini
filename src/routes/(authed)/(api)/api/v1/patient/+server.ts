4004354fe88aa70905dfa65ce0f2c60b6d5a6f83
hAbuMustafa <hosam5553@gmail.com>
2026-05-19 14:50:07 +0300

    patient lookup endpoint

---
+++ src/routes/(authed)/(api)/api/v1/patient/+server.ts
@@ -0,0 +1,44 @@
+import { db } from "$lib/server/db/";
+import { patients_view } from "$lib/server/db/schema";
+import { like, eq } from "drizzle-orm";
+import { json } from "@sveltejs/kit";
+import { fuzzyQuery } from "$lib/server/db/utils.js";
+
+export async function GET({ url }) {
+  const patientQuery = url.searchParams.get("q");
+
+  if (!patientQuery)
+    return new Response(null, { status: 400, statusText: "Bad Request" });
+
+  const queryType = /\p{Script=Arabic}/u.test(patientQuery)
+    ? "name"
+    : /^\d\d\/\d{1,5}$/.test(patientQuery)
+      ? "file_id"
+      : "id_number";
+
+  let patientMatches: PatientT[];
+
+  switch (queryType) {
+    case "name":
+      patientMatches = await db
+        .select()
+        .from(patients_view)
+        .where(like(patients_view.name, fuzzyQuery(patientQuery)));
+      break;
+    case "file_id":
+      patientMatches = await db
+        .select()
+        .from(patients_view)
+        .where(eq(patients_view.id, patientQuery));
+      break;
+    default:
+      patientMatches = await db
+        .select()
+        .from(patients_view)
+        .where(like(patients_view.id, `${patientQuery}%`));
+
+      break;
+  }
+
+  return json(patientMatches);
+}
