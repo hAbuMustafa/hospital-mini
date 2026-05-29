9dc49f2310609e0c6460962dadb3c3810ebaa3b9
hAbuMustafa <hosam5553@gmail.com>
2026-05-29 15:25:59 +0300

    add dates

--- src/routes/(authed)/invoice/create/[year=yy]/[patientId=fileNumber]/state.svelte.ts
+++ src/routes/(authed)/invoice/create/[year=yy]/[patientId=fileNumber]/state.svelte.ts
@@ -1,7 +1,11 @@
 export const invoiceData = $state<{
-  patient: PatientT | null;
-  staleData: StaleData | null;
+  patient: PatientT;
+  staleData: StaleData;
+  fromDateString: string;
+  toDateString: string;
 }>({
   patient: null,
   staleData: null,
+  fromDateString: "",
+  toDateString: "",
 });
