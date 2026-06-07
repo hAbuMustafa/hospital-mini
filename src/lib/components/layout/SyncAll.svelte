25a850d4ec580bc0754c08fe8b85003e218026b4
hAbuMustafa <hosam5553@gmail.com>
2026-06-07 15:46:10 +0300

    more sync buttons

--- src/lib/components/layout/SyncAll.svelte
+++ src/lib/components/layout/SyncAll.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import CloudSync from "@lucide/svelte/icons/cloud-sync";
+  import RefreshCwIcon from "@lucide/svelte/icons/refresh-cw";
+  import PersonIcon from "@lucide/svelte/icons/user-pen";
+  import DrugIcon from "@lucide/svelte/icons/pill";
 
   let loading = $state(false);
 </script>
@@ -23,7 +25,55 @@
         .catch((e) => console.error(e));
     }}
   >
-    <CloudSync />
+    <RefreshCwIcon size="2rem" />
+  </button>
+
+  <button
+    type="button"
+    aria-label="تحديث بيانات المرضى"
+    title="تحديث بيانات المرضى"
+    disabled={loading}
+    class:loading
+    onclick={() => {
+      loading = true;
+
+      fetch("/api/v1/sync/patients")
+        .then((r) => r.json())
+        .then((d) => {
+          console.log(d);
+          loading = false;
+        })
+        .catch((e) => console.error(e));
+    }}
+  >
+    <RefreshCwIcon size="2rem" />
+    <span class="embedded">
+      <PersonIcon size="16px" class="embedded" color="lightgreen" />
+    </span>
+  </button>
+
+  <button
+    type="button"
+    aria-label="تحديث بيانات الأدوية"
+    title="تحديث بيانات الأدوية"
+    disabled={loading}
+    class:loading
+    onclick={() => {
+      loading = true;
+
+      fetch("/api/v1/sync/drugs")
+        .then((r) => r.json())
+        .then((d) => {
+          console.log(d);
+          loading = false;
+        })
+        .catch((e) => console.error(e));
+    }}
+  >
+    <RefreshCwIcon size="2rem" />
+    <span class="embedded">
+      <DrugIcon size="16px" class="embedded" color="light-dark(maroon, salmon)" />
+    </span>
   </button>
 </div>
 
@@ -33,6 +83,7 @@
     background-color: transparent;
     border: none;
     cursor: pointer;
+    position: relative;
 
     :global(svg) {
       display: inline-block;
@@ -50,6 +101,16 @@
         animation: spin 1s linear infinite;
       }
     }
+  }
+
+  .embedded {
+    position: absolute;
+    inset: 0;
+
+    display: flex;
+
+    align-items: center;
+    justify-content: center;
   }
 
   @keyframes spin {
