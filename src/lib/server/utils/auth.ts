7f0e465acd5f01f130ca525c9b862d778453d3da
hAbuMustafa <hosam5553@gmail.com>
2026-06-11 00:46:21 +0300

    add secret to config

--- src/lib/server/utils/auth.ts
+++ src/lib/server/utils/auth.ts
@@ -5,6 +5,7 @@ import { db_auth } from "$lib/server/db/index-auth";
 import { PUBLIC_BETTER_AUTH_URL } from "$env/static/public";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { getRequestEvent } from "$app/server";
+import { BETTER_AUTH_SECRET } from "$env/static/private";
 
 export const auth = betterAuth({
   baseURL: PUBLIC_BETTER_AUTH_URL,
@@ -23,4 +24,5 @@ export const auth = betterAuth({
     sveltekitCookies(getRequestEvent),
   ],
   trustedOrigins: [PUBLIC_BETTER_AUTH_URL],
+  secret: BETTER_AUTH_SECRET,
 });
