be9355522bd6a6a71ceedcdd26f40932a4adc253
hAbuMustafa <hosam5553@gmail.com>
2026-06-25 02:17:27 +0300

    fix for orm complaint about numerics

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -82,7 +82,7 @@ export const transactions = sqliteTable("transactions", {
   id: int().primaryKey({ autoIncrement: true }),
   item_id: int().notNull(),
   qty: int().notNull(),
-  unit_price: numeric().notNull(),
+  unit_price: numeric({ mode: "number" }).notNull(),
   ticket_id: int().references(() => transactionTickets.id),
 });
 
