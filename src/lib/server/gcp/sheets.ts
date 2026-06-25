39318efc15817104d6cac4232b68b50ec8f1e8de
hAbuMustafa <hosam5553@gmail.com>
2026-06-25 04:31:22 +0300

    add batch get util

--- src/lib/server/gcp/sheets.ts
+++ src/lib/server/gcp/sheets.ts
@@ -1,4 +1,4 @@
-import { sheets } from "@googleapis/sheets";
+import { sheets, type sheets_v4 } from "@googleapis/sheets";
 import { GoogleAuth } from "google-auth-library";
 import { GOOGLE_CLIENT_EMAIL, GOOGLE_PRIVATE_KEY } from "$env/static/private";
 
@@ -33,6 +33,33 @@ export async function getSheetRange(spreadsheetId: string, range: string) {
     return response.data;
   } catch (error) {
     console.error("Error fetching sheet range:", error);
+    throw error;
+  }
+}
+
+/**
+ * Fetch multiple ranges from a specific Google Sheet
+ * @param spreadsheetId - The Google Sheet ID
+ * @param ranges - Array of ranges like ['Sheet1!A1:B10', 'Sheet2!C1:D20']
+ * @param majorDimension - Optional, defaults to 'ROWS'
+ * @returns Batch sheet data
+ */
+export async function getSheetRanges(spreadsheetId: string, ranges: string[]) {
+  try {
+    const response = await sheetsAPI.spreadsheets.values.batchGet({
+      spreadsheetId,
+      ranges,
+    });
+
+    const result: Record<string, sheets_v4.Schema$ValueRange> = {};
+
+    response.data.valueRanges?.forEach((vr) => {
+      if (vr.range) result[vr.range.split("!")[0]] = vr;
+    });
+
+    return result;
+  } catch (error) {
+    console.error("Error fetching sheet ranges:", error);
     throw error;
   }
 }
