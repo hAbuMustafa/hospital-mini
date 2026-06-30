af5f9454449b950ef70ae0a326962ed31322d358
hAbuMustafa <hosam5553@gmail.com>
2026-06-30 13:45:13 +0300

    cleanup

--- src/routes/(authed)/(api)/api/v1/drug/+server.ts
+++ src/routes/(authed)/(api)/api/v1/drug/+server.ts
@@ -2,7 +2,7 @@ import { db } from "$lib/server/db/";
 import { drugs } from "$lib/server/db/schema";
 import { fuzzyQuery as fuzzy } from "$lib/server/db/utils";
 import { json } from "@sveltejs/kit";
-import { like, or, sql } from "drizzle-orm";
+import { or, sql } from "drizzle-orm";
 
 export async function GET({ url }) {
   const query = url.searchParams.get("q");
@@ -19,8 +19,8 @@ export async function GET({ url }) {
         sql`${drugs.name_ar} LIKE ${fuzzyQuery} ESCAPE '\\'`,
         sql`${drugs.name} LIKE ${fuzzyQuery} ESCAPE '\\'`,
         sql`${drugs.tradename_ar} LIKE ${fuzzyQuery} ESCAPE '\\'`,
-        sql`${drugs.tradename} LIKE ${fuzzyQuery} ESCAPE '\\'`,
-      ),
+        sql`${drugs.tradename} LIKE ${fuzzyQuery} ESCAPE '\\'`
+      )
     );
 
   return json(matches);
