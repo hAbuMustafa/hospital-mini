ab3ede4660d66f45827149c94b0fde62448f453e
hAbuMustafa <hosam5553@gmail.com>
2026-07-13 15:18:27 +0300

    utility fn for constants

--- src/lib/CONSTANTS.ts
+++ src/lib/CONSTANTS.ts
@@ -3,3 +3,11 @@ export const nonDivisibleBoxes = [
   { id: 166, min: 30 },
   { id: 198, min: 60 },
 ];
+
+export function isNarcotic(drugId: number) {
+  return narcoticsIds.includes(drugId);
+}
+
+export function unacceptedAmount(drugId: number, amount: number) {
+  return nonDivisibleBoxes.some((b) => b.id === drugId && amount % b.min !== 0);
+}
