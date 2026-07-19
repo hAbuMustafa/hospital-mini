46aec6c8a9be031cb53cd02e5aabef02be1e7660
hAbuMustafa <hosam5553@gmail.com>
2026-07-19 23:33:12 +0300

    add customSessionClient plugin

--- src/lib/auth-client/index.ts
+++ src/lib/auth-client/index.ts
@@ -1,9 +1,9 @@
 import { dev } from "$app/environment";
 import { PUBLIC_ORIGIN, PUBLIC_ORIGIN_PROD } from "$env/static/public";
 import { createAuthClient } from "better-auth/client";
-import { adminClient } from "better-auth/client/plugins";
+import { adminClient, customSessionClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: dev ? PUBLIC_ORIGIN : PUBLIC_ORIGIN_PROD,
-  plugins: [adminClient()],
+  plugins: [adminClient(), customSessionClient()],
 });
