6dd6868b514bb0199774593abf1febcc84e5d343
hAbuMustafa <hosam5553@gmail.com>
2026-08-09 13:56:14 +0300

    Authorization Filter Component

---
+++ src/lib/components/AuthzFilter.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { authState } from "$lib/auth-client/auth.svelte";
+  import type { Snippet } from "svelte";
+
+  type PropsT = {
+    allowRole?: string[];
+    allowUser?: string[];
+    allowDepartment?: number[];
+
+    blockRole?: string[];
+    blockUser?: string[];
+    blockDepartment?: number[];
+
+    children: Snippet;
+  };
+
+  const {
+    allowRole = [],
+    allowUser = [],
+    allowDepartment = [],
+
+    blockRole = [],
+    blockUser = [],
+    blockDepartment = [],
+
+    children,
+  }: PropsT = $props();
+
+  function canSee(user: typeof authState.user) {
+    if (user?.role === "admin") return true;
+
+    return (
+      user &&
+      user.role &&
+      (allowDepartment.includes(user?.affiliation) ||
+        allowRole.includes(user?.role) ||
+        allowUser.includes(user.id)) &&
+      (!blockDepartment.includes(user?.affiliation) ||
+        !blockRole.includes(user.role) ||
+        !blockUser.includes(user.id))
+    );
+  }
+</script>
+
+{#if canSee(authState.user)}
+  {@render children()}
+{/if}
