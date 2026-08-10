f6103717a9833d02e6f53c9401affe12074d1380
hAbuMustafa <hosam5553@gmail.com>
2026-08-10 14:53:54 +0300

    dialog component

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let { id, children } = $props();
+</script>
+
+<dialog {id} closedby="any">
+  {@render children()}
+</dialog>
+
+<style>
+  dialog {
+    border-color: var(--main-text-color);
+    border-radius: 8px;
+
+    &::backdrop {
+      background-color: black;
+      opacity: 0.5;
+    }
+  }
+</style>
