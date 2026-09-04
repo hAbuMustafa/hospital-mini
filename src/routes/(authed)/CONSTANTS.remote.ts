c46528c24a5851d05dd920cb8cd80d1e154c11ea
hAbuMustafa <hosam5553@gmail.com>
2026-09-04 21:02:38 +0300

    change first date source

--- src/routes/(authed)/CONSTANTS.remote.ts
+++ src/routes/(authed)/CONSTANTS.remote.ts
@@ -1,9 +1,11 @@
 import { query } from "$app/server";
 import { db } from "$lib/server/db";
-import { transactionTickets } from "$lib/server/db/schema";
+import { status } from "$lib/server/db/schema";
+import { eq } from "drizzle-orm";
 
 export const getSystemFirstDate = query(async () => {
-  return (
-    (await db.select().from(transactionTickets).limit(1))?.[0].timestamp ?? new Date()
+  return new Date(
+    (await db.select().from(status).where(eq(status.item, "first_date")))?.[0]?.value ??
+      new Date()
   );
 });
