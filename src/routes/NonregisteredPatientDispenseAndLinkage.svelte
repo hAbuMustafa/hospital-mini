d7fdd011feb4a1c1238fff16fd9e0be9925bc513
hAbuMustafa <hosam5553@gmail.com>
2026-09-09 03:00:00 +0300

    notifier component

---
+++ src/routes/NonregisteredPatientDispenseAndLinkage.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { getUnlinkedTickets } from "./(authed)/pharmacy/ticket.remote";
+
+  const unlinkedTickets = await getUnlinkedTickets();
+</script>
+
+{#if unlinkedTickets.length}
+  <p class="warning message">
+    لديك {unlinkedTickets.length} تذكرة لم يتم ربطها بمرضى الأقسام. يرجى
+    <a href="/pharmacy/link-tickets">ربطها الآن</a>.
+  </p>
+{/if}
