a38d9f7aac046a96983696ff35977ac3f39e9ad8
hAbuMustafa <hosam5553@gmail.com>
2026-09-09 03:36:05 +0300

    include new components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
   import { authState } from "$lib/auth-client/auth.svelte";
   import UnsyncedNarcotics from "./UnsyncedNarcotics.svelte";
+  import NonregisteredPatientDispenseAndLinkage from "./NonregisteredPatientDispenseAndLinkage.svelte";
+  import DispenseToUnregisteredPatient from "./DispenseToUnregisteredPatient.svelte";
 </script>
 
 <h1>مستشفى 23 يوليو للأمراض الصدرية</h1>
 
+{#if authState.user?.role?.includes("-ph-")}
+  <NonregisteredPatientDispenseAndLinkage />
+{/if}
+
 {#if authState.user?.affiliation === 1}
+  <DispenseToUnregisteredPatient />
   <UnsyncedNarcotics />
 {/if}
