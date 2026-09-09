9dac0570595101b83ec8ebc440975505ef9da135
hAbuMustafa <hosam5553@gmail.com>
2026-09-09 03:23:23 +0300

    create flow for unregistered patient dispense

---
+++ src/routes/DispenseToUnregisteredPatient.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let patientName = $state("");
+</script>
+
+<h2>صرف لمريض غير مسجل</h2>
+
+<form action="/dispense/26/0" method="GET">
+  <input
+    type="text"
+    name="patient_name"
+    bind:value={patientName}
+    placeholder="اسم المريض"
+  />
+  <input type="submit" class="btn" value="صرف تذكرة" />
+</form>
+
+<style>
+  .btn {
+    --bg: green;
+  }
+</style>
