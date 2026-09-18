14a4723b4073a8d468319ce426203f3ef555f021
hAbuMustafa <hosam5553@gmail.com>
2026-09-18 14:53:09 +0300

    ticket number component

---
+++ src/lib/components/TicketNumber.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let { number } = $props();
+</script>
+
+<a class="ticket-id" href="/pharmacy/ticket/{number}">
+  &#x23;{number}
+</a>
+
+<style>
+  .ticket-id {
+    color: gray;
+    text-decoration: none;
+  }
+</style>
